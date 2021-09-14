ad2a04edab09478becc1e4f30bcc7d1a70f705a3
Toby Whitehead <whiteheadt99@gmail.com>
2021-09-14 16:07:27 +0100

    createDeck should be working now for 52 cards

--- src/main/java/com/blackjack/Deck.java
+++ src/main/java/com/blackjack/Deck.java
@@ -23,6 +23,60 @@ public class Deck {
             }
             deck.push(card);
         }
+        for(int i = 1; i < 14; i++){
+            Card card = new Card();
+            card.setCardSuit("diamonds");
+            card.setCardValue(i);
+            if(i > 10) {
+                card.setFaceCard(true);
+            }
+            if(i == 11) {
+                card.setFaceCardType("jack");
+            }
+            if(i == 12) {
+                card.setFaceCardType("queen");
+            }
+            if(i == 13) {
+                card.setFaceCardType("king");
+            }
+            deck.push(card);
+        }
+        for(int i = 1; i < 14; i++){
+            Card card = new Card();
+            card.setCardSuit("clubs");
+            card.setCardValue(i);
+            if(i > 10) {
+                card.setFaceCard(true);
+            }
+            if(i == 11) {
+                card.setFaceCardType("jack");
+            }
+            if(i == 12) {
+                card.setFaceCardType("queen");
+            }
+            if(i == 13) {
+                card.setFaceCardType("king");
+            }
+            deck.push(card);
+        }
+        for(int i = 1; i < 14; i++){
+            Card card = new Card();
+            card.setCardSuit("spades");
+            card.setCardValue(i);
+            if(i > 10) {
+                card.setFaceCard(true);
+            }
+            if(i == 11) {
+                card.setFaceCardType("jack");
+            }
+            if(i == 12) {
+                card.setFaceCardType("queen");
+            }
+            if(i == 13) {
+                card.setFaceCardType("king");
+            }
+            deck.push(card);
+        }
     return(deck);
     }
 }
